1f93364b60660c4622513c19e18b96b43bb9132e
Ayush Kumar <aayushkumar1228@gmail.com>
2025-06-25 19:05:32 +0530

    Implement the About section

--- src/components/Cocktails.jsx
+++ src/components/Cocktails.jsx
@@ -27,7 +27,7 @@ const Cocktails = () => {
     }, [])
 
     return (
-        <section id='cocktails' className='noisy'>
+        <section id='cocktails'>
             <img src='/images/cocktail-left-leaf.png' alt='left-leaf' id='c-left-leaf' />
             <img src='/images/cocktail-right-leaf.png' alt='right-leaf' id='c-right-leaf' />
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -67,7 +67,7 @@ const Hero = () => {
 
     return (
         <>
-            <section id='hero' className='noisy'>
+            <section id='hero'>
                 <h1 className='title uppercase'>Mojito</h1>
 
                 <img src='/images/hero-left-leaf.png' alt='left-leaf' className='left-leaf' />
